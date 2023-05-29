f57f9f5811f6363e098eed7e5cd7c424f9931926
Michał Kostrzewski <michal.kostrzewski@protonmail.com>
2023-05-29 11:02:30 +0200

    Rework Handler

--- src/connection/connection.hpp
+++ src/connection/connection.hpp
@@ -1,29 +1,29 @@
 #pragma once
 
+#include "database/entry_guard.hpp"
 #include "db_guard.hpp"
 #include "mq.hpp"
 #include "packet.hpp"
 #include <boost/asio/awaitable.hpp>
 #include <boost/asio/co_spawn.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/spawn.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/address.hpp>
 #include <boost/asio/use_future.hpp>
+#include <boost/thread/future.hpp>
+#include <memory>
 #include <optional>
 
-class Handler
-{
-public:
-    Handler(boost::asio::io_context& ctx, std::unique_ptr<Stream>&& stream, DBGuard db_guard)
-        : m_ctx{ctx}, m_stream{std::move(stream)}, m_db_guard(db_guard)
-    {
-        
-    }
+// TODO: move definitions to .cpp (and remove inline)
 
-    auto handle_incoming() -> boost::asio::awaitable<void>
+namespace
+{
+    inline auto handle_incoming(std::shared_ptr<Stream> stream, std::shared_ptr<EntryGuard> ip_guard, DBGuard db_guard) -> boost::asio::awaitable<void>
     {
         while(true) // TODO shutdown
         {
-            auto packet_option = co_await Packet::from_stream(*m_stream);
+            auto packet_option = co_await Packet::from_stream(*stream);
             if(packet_option.has_value() == false)
             {
                 std::cerr << "Error in handle_incoming()" << std::endl;
@@ -32,7 +32,7 @@ public:
 
             auto packet = packet_option.value();
 
-            auto mq_tx_option = m_db_guard.get(packet.dst_address());
+            auto mq_tx_option = db_guard.get(packet.dst_address());
             if(mq_tx_option.has_value() == false)
             {
                 continue;
@@ -43,12 +43,12 @@ public:
         }
     }
 
-    auto handle_outgoing(Receiver mq_rx) -> boost::asio::awaitable<void>
+    inline auto handle_outgoing(std::shared_ptr<Stream> stream, std::shared_ptr<EntryGuard> ip_guard, Receiver mq_rx) -> boost::asio::awaitable<void>
     {
         while(true) // TODO shutdown
         {
             auto packet = co_await mq_rx.async_receive();
-            auto written_option = co_await m_stream->write(packet.data(), packet.size());
+            auto written_option = co_await stream->write(packet.data(), packet.size());
             if(written_option.has_value() == false)
             {
                 std::cerr << "Error in handle_outgoing()" << std::endl;
@@ -56,22 +56,21 @@ public:
             }
         }
     }
+}
 
-    auto run() -> void
+inline auto handle_client(boost::asio::io_context& ctx, std::shared_ptr<Stream>&& stream, DBGuard db_guard) -> bool
+{
+    auto register_option = db_guard.register_without_ip();
+    if(register_option.has_value() == false)
     {
-        auto register_option = m_db_guard.register_without_ip();
-        if(register_option.has_value() == false)
-        {
-            return;
-        }
-
-        auto[ip_guard, mq_rx] = std::move(register_option.get());
-
-        auto future_rx = boost::asio::co_spawn(m_ctx, handle_incoming(), boost::asio::use_future);
+        return false;
     }
 
-private:
-    boost::asio::io_context& m_ctx;
-    std::unique_ptr<Stream> m_stream;
-    DBGuard m_db_guard;
-};
+    auto[ip_guard, mq_rx] = std::move(register_option.get());
+    auto ip_guard_ptr = std::make_shared<EntryGuard>(std::move(ip_guard));
+
+    boost::asio::co_spawn(ctx, handle_incoming(stream, ip_guard_ptr, db_guard), boost::asio::detached);
+    boost::asio::co_spawn(ctx, handle_outgoing(stream, ip_guard_ptr, mq_rx), boost::asio::detached);
+
+    return true;
+}
